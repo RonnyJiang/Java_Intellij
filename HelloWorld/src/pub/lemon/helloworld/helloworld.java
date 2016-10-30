7bb5c542ddd0337e95cb867e8a70b2f93bf1f561
Ronny <set@aliyun.com>
2016-10-30 17:09:22 +0800

    add module: HelloWorld

---
+++ HelloWorld/src/pub/lemon/helloworld/helloworld.java
@@ -0,0 +1,10 @@
+package pub.lemon.helloworld;
+
+/**
+ * Created by Ronny on 2016/10/30.
+ */
+public class helloworld {
+    public  static void main(String [] args){
+        System.out.println("Hello world ronny,use lemon package");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Java_Intellij
 Java project （USED IED:Intellij IDEA）
+
+module [1] helloworld 2016-10-30
